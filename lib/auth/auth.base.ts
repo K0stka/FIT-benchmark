e24da938ee0d6a46a4c4a521e44609a21bd1afa1
K0stka <www.honza.90@gmail.com>
2025-11-16 01:37:39 +0100

    Fix auth

--- lib/auth/auth.base.ts
+++ lib/auth/auth.base.ts
@@ -3,6 +3,9 @@ import NextAuth from "next-auth";
 import { User } from "../types";
 
 export const AuthBase = {
+	pages: {
+		signIn: "/login",
+	},
 	callbacks: {
 		async jwt({ token, user }) {
 			if (user) {
